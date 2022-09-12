f775b2138c59055f62effc22bb99d6084b0f4fb4
Roland Mackintosh <rolandmackintosh@outlook.com>
2022-09-11 18:04:40 -0700

    successful retrieval of viewport width

--- scrapers/rentCanada/app.py
+++ scrapers/rentCanada/app.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import json
 from flask import Flask, request, make_response
 
 print("cats")
@@ -13,7 +14,7 @@ from util.checker import check_public_ip
 
 
 
-@app.route("/")
+@app.route("/", methods=["POST"])
 def apartments():
     """
     Uses lat and long to query RentCanada.com for apartments.
@@ -21,8 +22,10 @@ def apartments():
     NOTE: In RentCanada's coordinate system, West is negative, East is positive.
     :return: A list of gyms.
     """
+    # print("trying to return cats")
+    # return "cats"
     proxy_ip, proxy_port = get_proxy_ip(0)
-    
+
     username = os.environ.get("username2")
     password = os.environ.get("password2")
     proxy_string = f"http://{username}:{password}@{proxy_ip}:{proxy_port}"
@@ -32,13 +35,23 @@ def apartments():
     # https_proxy_string = "https://" + str(proxy_ip) + ":" + str(proxy_port)
     # proxy = {"http": http_proxy_string, "https": http_proxy_string}
     public_ip1, public_ip2 = check_public_ip(proxy_dict)
-    print(public_ip1,public_ip2)
+    print(public_ip1, proxy_ip)
+    print(public_ip2, proxy_ip)
+    print("===============================")
     if public_ip1 == proxy_ip and public_ip2 == proxy_ip:
         pass
     else:
+        print("error!!!!")
         print(public_ip1, proxy_ip)
         raise NotImplementedError("The expected proxy IP was different from public IP")
-    location = request.json
+
+    print("++++++++++++++")
+    print(request)
+    # location = request.get_json()
+    location = request.get_json()
+    print(location, "45rm")
+    print(type(location), "50rm")
+    print(str(location), "51rm")
     # city = location["city"]
     # state = location["state"]
     # country = location["country"]
@@ -60,17 +73,28 @@ def apartments():
     headers = {
         'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36',
     }
+    print("sending request")
     r = s.get(start, headers=headers)
 
     results = r.json()
-    # print(text.json(), "19rm")
 
-    # print(text.count("propertyId"))
     print(lat, long, len(results))
     return results
 
 
 def make_query_string(lat1, long1, lat2, long2):
+    return (
+        f"https://www.rentcanada.com/api/map-markers?cityId=74&filters=%7B%22amenities%22:%7B%22checklist%22:[]"
+        f"%7D,%22utilities%22:%7B%22checklist%22:[]%7D,%22petPolicies%22:%7B%22checklist%22:[]%7D,%22rentalTypes"
+        f"%22:%7B%22checklist%22:[]%7D,%22beds%22:%7B%22min%22:null,%22max%22:null,%22checklist%22:[]%7D,%22baths"
+        f"%22:%7B%22min%22:null,%22max%22:null,%22checklist%22:[]%7D,%22rates%22:%7B%22min%22:null,%22max%22:null"
+        f"%7D,%22sqft%22:%7B%22min%22:null,%22max%22:null%7D,%22mapBounds%22:%7B%22north%22:%7B"
+        f"%22lat%22:null,%22lng%22:null%7D,%22south%22:%7B%22lat%22:null,%22lng%22:null%7D%7D,%22keyword%22:null,"
+        f"%22furnished%22:null%7D"
+    )
+
+
+def old_make_query_string(lat1, long1, lat2, long2):
     # according to https://www.w3schools.com/tags/ref_urlencode.ASP
     # %22 = a double quote like "
     return (f"https://www.rentcanada.com/api/map-markers?filters=%7B%22amenities%22:%7B%22checklist%22:[]"
